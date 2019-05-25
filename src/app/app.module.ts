33955e1b6ddbe253f859dcbfa9cb2a62a8399cef
Andre Silva <abrandao@tutanota.com>
2019-05-25 19:17:27 +0300

    Correct errors regarding multiple component declarations.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+import { PortfolioModule } from './portfolio/portfolio.module';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { SplashComponent } from './splash/splash.component';
@@ -24,7 +25,8 @@ import { RomanComponent } from './roman/roman/roman.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    PortfolioModule
   ],
   exports: [
     NavbarComponent,
--- src/app/portfolio/portfolio.module.ts
+++ src/app/portfolio/portfolio.module.ts
@@ -1,14 +1,10 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { ItemsComponent } from './items/items.component';
-import { AppModule } from '../app.module';
-import { ItemComponent } from './item/item.component';
 
 @NgModule({
-  declarations: [ItemsComponent, ItemComponent],
+  declarations: [],
   imports: [
-    CommonModule,
-    AppModule
+    CommonModule
   ]
 })
 export class PortfolioModule { }
