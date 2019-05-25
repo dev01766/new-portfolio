4dd92f63452e8e1a5f7774e7316a4e799d9236d9
Andre Silva <abrandao@tutanota.com>
2019-05-25 19:20:21 +0300

    Correct errors regarding multiple component declarations. [2]

--- src/app/roman/roman.module.ts
+++ src/app/roman/roman.module.ts
@@ -1,13 +1,10 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { RomanComponent } from './roman/roman.component';
-import { AppModule } from '../app.module';
 
 @NgModule({
-  declarations: [RomanComponent],
+  declarations: [],
   imports: [
-    CommonModule,
-    AppModule
+    CommonModule
   ]
 })
 export class RomanModule { }
